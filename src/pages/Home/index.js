afbe10081af241fe36518e511d8a2288942b91ae
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-11-01 14:25:29 +0100

    Ajout data-testid pour test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -51,14 +51,14 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section className="EventsContainer" data-testid="list-of-events">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
       <section className="PeoplesContainer">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
-        <div className="ListContainer">
+        <div className="ListContainer" data-testid="people-card-test">
           <PeopleCard
             imageSrc="/images/stephanie-liverani-Zz5LQe-VSMY-unsplash.png"
             name="Samira"
@@ -113,8 +113,8 @@ const Page = () => {
         </Modal>
       </div>
     </main>
-    <footer className="row">
-      <div className="col presta">
+    <footer className="row" data-testid="footer">
+      <div className="col presta" data-testid="last-event-card">
         <h3>Notre derniére prestation</h3>
         <EventCard
           imageSrc={last?.cover}
