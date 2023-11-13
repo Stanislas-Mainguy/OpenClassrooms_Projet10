31c820d537e0aa6dd338f48f340ace7e7990c221
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-11-13 16:12:59 +0100

    tri par date

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -1,11 +1,5 @@
 import PropTypes from "prop-types";
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { createContext, useCallback, useContext, useEffect, useState, useMemo } from "react";
 
 const DataContext = createContext({});
 
@@ -19,26 +13,34 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+
   const getData = useCallback(async () => {
     try {
-      setData(await api.loadData());
+      const loadedData = await api.loadData();
+      if (loadedData && loadedData.events) {
+        loadedData.events.sort((a, b) => new Date(b.date) - new Date(a.date));
+      }
+      setData(loadedData);
     } catch (err) {
       setError(err);
     }
   }, []);
+
   useEffect(() => {
     if (data) return;
     getData();
-  });
-  
+  }, [data, getData]);
+
+  const lastEvent = data?.events?.length > 0 ? data.events[0] : null;
+
+  const contextValue = useMemo(() => ({
+    data,
+    error,
+    last: lastEvent,
+  }), [data, error, lastEvent]);
+
   return (
-    <DataContext.Provider
-      // eslint-disable-next-line react/jsx-no-constructed-context-values
-      value={{
-        data,
-        error,
-      }}
-    >
+    <DataContext.Provider value={contextValue}>
       {children}
     </DataContext.Provider>
   );
@@ -46,7 +48,7 @@ export const DataProvider = ({ children }) => {
 
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export const useData = () => useContext(DataContext);
 
